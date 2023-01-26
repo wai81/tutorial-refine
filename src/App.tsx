c65033f82d24e48f7e9074db3a6f9817a081db13
wai81 <50743966+wai81@users.noreply.github.com>
2023-01-26 12:13:50 +0300

    added products

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import { Refine } from "@pankod/refine-core";
+import { Refine } from '@pankod/refine-core';
 import {
   notificationProvider,
   RefineSnackbarProvider,
@@ -11,24 +11,34 @@ import {
   LightTheme,
   ReadyPage,
   ErrorComponent,
-} from "@pankod/refine-mui";
+} from '@pankod/refine-mui';
 
-import dataProvider from "@pankod/refine-simple-rest";
-import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from '@pankod/refine-simple-rest';
+import routerProvider from '@pankod/refine-react-router-v6';
+import { MuiInferencer } from '@pankod/refine-inferencer/mui';
 
 function App() {
   return (
     <ThemeProvider theme={LightTheme}>
       <CssBaseline />
-      <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+      <GlobalStyles styles={{ html: { WebkitFontSmoothing: 'auto' } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+          dataProvider={dataProvider('https://api.fake-rest.refine.dev')}
           notificationProvider={notificationProvider}
           Layout={Layout}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
           routerProvider={routerProvider}
+          resources={[
+            {
+              name: 'products',
+              list: MuiInferencer,
+              create: MuiInferencer,
+              show: MuiInferencer,
+              edit: MuiInferencer,
+            },
+          ]}
         />
       </RefineSnackbarProvider>
     </ThemeProvider>
